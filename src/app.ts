d8ba7587f29b7fee10539e800ecef686378ace73
idsmoura <cleophasbarwareng@ids.company>
2022-09-27 06:21:01 +0300

    fix: cors issues, latest Era Rewards

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,3 @@ import 'module-alias/register';
 import 'dotenv/config';
 import { Init } from '@config';
 Init();
-
-// TODO find address type
-// 01098d1758f1ca75350dfec8a1c4c1984a88d1ea5eab5590fbc9e856d67cde31eb
